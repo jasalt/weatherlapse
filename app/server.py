b52d65dbf822c5325ac7dca598e584907c063d2f
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-09-27 13:57:01 +0300

    Lisätty template koti- ja päivä-sivuille.

--- app/server.py
+++ app/server.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def view_home():
-    return "TODO tervehdi käyttäjää ja näytä kalenteri. \nViimeisin päivä olisi myös mukava näyttää tässä."
+    return render_template('home.html')
 
 
-@app.route("/day/<day>")
-def view_day(day):
-    return "Näkymä päivälle " + day
+@app.route("/day/<date>")
+def view_day(date):
+    return render_template('day.html', day=date)
 
 
 if __name__ == "__main__":
