c0a41a602fb7fa2ef0a6472f8c6ca00a7acfc7f1
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-12-31 12:44:15 +0200

    Change playlist to a new.

--- app/youtube.py
+++ app/youtube.py
@@ -1,5 +1,5 @@
 # Load data for videos in Youtube playlist
-# https://www.youtube.com/playlist?list=PLy7eek8wTbV9OtrbY3CJo5mRWnhuwTen0
+# Uses video title as date, formatted as 20151230
 
 ## TODO handle videos for multiple months
 # all_videos dict contains videos by day.
@@ -12,7 +12,7 @@ from datetime import datetime
 
 print("Loading Youtube video playlist")
 
-playlist = pafy.get_playlist("PLy7eek8wTbV9OtrbY3CJo5mRWnhuwTen0")
+playlist = pafy.get_playlist("PLXSngHQzQiiI8DyrElGi_N_rv-8ToJvIT")
 videos = playlist['items']
 all_videos = {}
 
@@ -20,7 +20,7 @@ for vid in videos:
     # Date is stored in video title
     vid_date_str = vid['playlist_meta']['title']
     
-    vid_day = datetime.strptime(vid_date_str, "%Y-%m-%d")
+    vid_day = datetime.strptime(vid_date_str, "%Y%m%d")
     
     year, month, day = vid_day.year, vid_day.month, vid_day.day
 
@@ -32,4 +32,4 @@ for vid in videos:
     try:
         all_videos[year][month][day] = vid['pafy']
     except:
-        print("Cant add " + vid_title)
+        print("Cant add " + vid_date_str)
